56e4d9df8b85584cd045a83f310881d67699b9cc
kkome <exazunyan1@gmail.com>
2024-03-15 17:53:06 +0900

    24/03/15

---
+++ ch10/src/main/java/kr/co/ch10/controller/MainController.java
@@ -0,0 +1,27 @@
+package kr.co.ch10.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+        return "/index";
+    }
+
+    @GetMapping("/user/login")
+    public String login(){
+        return "/user/login";
+    }
+
+    @GetMapping("/user/list")
+    public String list(){
+        return "/user/list";
+    }
+}
